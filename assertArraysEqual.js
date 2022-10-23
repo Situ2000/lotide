b018c63d181bbcc580a7fd5430e1894f0d75b756
Situ2000 <yuanyuansitu@gmail.com>
2022-10-23 13:37:01 +0000

    Implement assertArraysEqual assignment

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("✅✅✅They're the same array.")
+  } else {
+    console.log("❌❌❌They're different array.")
+  }
+};
+
+//Test codes
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
+assertArraysEqual([1, 3, 3], [1, 2, 3]);
