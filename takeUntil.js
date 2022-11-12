1de938fa282c60c39700825368f69cf9a78c0be9
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:17:41 +0000

    final version of takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require("./assertArraysEqual");
+
 const takeUntil = (array, callback) => {
   const results = [];
   for (let i = 0; i < array.length; i++) {
@@ -6,29 +8,6 @@ const takeUntil = (array, callback) => {
     } else {
       results.push(array[i]);
     }
-  }
-};
-
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  if (eqArrays(actual, expected) === true) {
-    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
