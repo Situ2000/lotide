453c5aac351d99c04cd75d4d200205e7770e0120
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 20:54:49 +0000

    final version of assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -5,6 +5,4 @@ const assertEqual = function(actual, expected) {
   console.log(actual === expected ? assertionPassed : assertionFailed);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
