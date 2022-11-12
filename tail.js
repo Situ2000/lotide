0cea878bbb305c1572ec2a534166f8ab59b6f952
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:15:03 +0000

    final version of tail

--- tail.js
+++ tail.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let assertionPassed = `💚💚💚Assertion Passed: ${actual} === ${expected}`;
-  let assertionFailed = `🅾️🅾️🅾️Assertion Failed: ${actual} !== ${expected}`;
-  console.log(actual === expected ? assertionPassed : assertionFailed);
-};
-
 const tail = function(arr) {
   let result = [];
   for (let i = 1; i < arr.length; i++) {
@@ -12,8 +6,4 @@ const tail = function(arr) {
   return result;
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
