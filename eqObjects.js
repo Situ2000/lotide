de384aa2e3bc9e2f997783fe845fbb0336181c26
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:04:59 +0000

    final version of eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,52 +1,50 @@
-const assertEqual = function(actual, expected) {
-  let assertionPassed = `💚💚💚Assertion Passed: ${actual} === ${expected}`;
-  let assertionFailed = `🅾️🅾️🅾️Assertion Failed: ${actual} !== ${expected}`;
-  console.log(actual === expected ? assertionPassed : assertionFailed);
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else {
-    for (let key in object1) {
+  }
+
+  for (let key in object1) {
+    if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (! eqArrays(object1[key], object2[key])) {
           return false;
         }
       } else {
-        if (object1[key] !== object2[key]) {
+        if (! eqObjects(object1[key], object2[key])) {
           return false;
         }
       }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
     }
-    return true;
   }
+
+  return true;
 };
 
-//Test Codes
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+module.exports = eqObjects;
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+// const eqObjects = function(object1, object2) {
+  // if (Object.keys(object1).length !== Object.keys(object2).length) {
+  //   return false;
+  // }
+
+//     for (let key in object1) {
+//       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+//         if (! eqArrays(object1[key], object2[key])) {
+//           return false;
+//         }
+//       } else {
+//         if (object1[key] !== object2[key]) {
+//           return false;
+//         }
+//       }
+//     }
+//     return true;
+// };
