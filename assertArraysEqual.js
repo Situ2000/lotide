acdc276fa8ec30bfa13afb45bbfab8c523eb5083
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 20:52:42 +0000

    final version of assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -20,7 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-//Test codes
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
-assertArraysEqual([1, 3, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
