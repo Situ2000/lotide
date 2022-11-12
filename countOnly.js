61a9eba084c51c32462db3c769b7e2880d03af93
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:03:04 +0000

    final version of countOnly

--- countOnly.js
+++ countOnly.js
@@ -1,21 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let assertionPassed = `💚💚💚Assertion Passed: ${actual} === ${expected}`;
-  let assertionFailed = `🅾️🅾️🅾️Assertion Failed: ${actual} !== ${expected}`;
-  console.log(actual === expected ? assertionPassed : assertionFailed);
-};
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
@@ -32,10 +14,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
