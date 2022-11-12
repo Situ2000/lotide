6a192a9a460c11ccd9c73c7e526f9b0c915799af
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:08:43 +0000

    final version of flatten

--- flatten.js
+++ flatten.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log("✅✅✅They're the same array.")
-  } else {
-    console.log("❌❌❌They're different array.")
-  }
-};
-
 const flatten = function(nestedArray) {
   let result = [];
   for (let i = 0; i < nestedArray.length; i++) {
@@ -33,8 +12,4 @@ const flatten = function(nestedArray) {
   return result;
 };
 
-//Test code
-console.log(flatten([1, 2, 3]));
-console.log(flatten([1, 2, [1, 2, 3], 3, 4]));
-console.log(flatten([[1, 3], [1, 2]]));
-console.log(flatten([1, [2, 3], [4, 5]]));
+module.exports = flatten;
