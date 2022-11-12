819be0490c96e916c931bf538fbfc26f26255356
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:19:44 +0000

    final version of without

--- without.js
+++ without.js
@@ -1,24 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log("✅✅✅They're the same array.")
-  } else {
-    console.log("❌❌❌They're different array.")
-  }
-};
-
+const assertArraysEqual = require('./assertArraysEqual');
+  
 const without = function(source, itemsToRemove) {
   let finalList = source.slice(0);
   for (let i = 0; i < finalList.length; i++) {
@@ -33,6 +14,10 @@ const without = function(source, itemsToRemove) {
 
 //Test code
 console.log(without([1, 2, 3], [1, 2, 3]));
+assertArraysEqual(without([1, 2, 3], [1, 2, 3]), []);
 console.log(without([1, 2, 3, 4], [1, 2, 3]));
+assertArraysEqual(without([1, 2, 3, 4], [1, 2, 3]), [4]);
 console.log(without([1, 3], [1, 2]));
+assertArraysEqual(without([1, 3], [1, 2]), [3]);
 console.log(without([1, 2, 3, 4], []));
+assertArraysEqual(without([1, 2, 3, 4], []), [1, 2, 3, 4]);
