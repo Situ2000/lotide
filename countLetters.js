e5d6518ed05e6d627dac512a42d854ba9ea0194e
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:01:32 +0000

    final version of countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let assertionPassed = `💚💚💚Assertion Passed: ${actual} === ${expected}`;
-  let assertionFailed = `🅾️🅾️🅾️Assertion Failed: ${actual} !== ${expected}`;
-  console.log(actual === expected ? assertionPassed : assertionFailed);
-};
-
 const countLetters = function(sampleString) {
   modifiedString = sampleString.replaceAll(' ', '');
   const results = {};
