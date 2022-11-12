f8140ae2aabfbe9840638af64a154f7ccb76007d
Situ2000 <yuanyuansitu@gmail.com>
2022-11-12 21:07:58 +0000

    final version of findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let assertionPassed = `💚💚💚Assertion Passed: ${actual} === ${expected}`;
-  let assertionFailed = `🅾️🅾️🅾️Assertion Failed: ${actual} !== ${expected}`;
-  console.log(actual === expected ? assertionPassed : assertionFailed);
-};
-
 const findKeyByValue = function(sampleObject, value) {
   for (const key in sampleObject) {
     if (sampleObject[key] === value) {
@@ -12,13 +6,4 @@ const findKeyByValue = function(sampleObject, value) {
   }
 };
 
-// Test Codes
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+module.exports = findKeyByValue;
