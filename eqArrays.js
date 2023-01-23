dca7f36f99386088799bc8fbd1b4f5a4ebcc12d8
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 09:34:25 +0000

    Add comments for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,18 @@
 const eqArrays = function(arr1, arr2) {
+  // Check whether the lengths of two arrays are equal
   if (arr1.length !== arr2.length) {
     return false;
   }
     
   for (let i = 0; i < arr1.length; i++){
+    // Check whether the items in both arrays are arrays
     if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // If they are arrays, they need to go through the function again
       if (! eqArrays(arr1[i], arr2[i])) {
         return false;
       }
     } else {
+      // If they are not arrays, compare whether these two items equal
       if (arr1[i] !== arr2[i]) {
         return false;
       }
