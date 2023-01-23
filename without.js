07e21b6c1a7d1540e3f77c21dc817000033e6f71
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 10:17:02 +0000

    Add comments for without

--- without.js
+++ without.js
@@ -1,14 +1,18 @@
 const assertArraysEqual = require('./assertArraysEqual');
   
 const without = function(source, itemsToRemove) {
+  // Copy source array, so that it will not modify the source
   let finalList = source.slice(0);
+
   for (let i = 0; i < finalList.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
+      // Compare each of the item in each of the array, if equal, remove the one in finalList
       if (finalList[i] === itemsToRemove[j]) {
         finalList.splice(i, 1);
       }
     }
   }
+
   return finalList;
 };
 
