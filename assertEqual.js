3c112214d6f762f44445afbf0b86786385f08c7c
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 06:29:08 +0000

    Add comments for assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+// Print out to the console that whether two values are equal
 const assertEqual = function(actual, expected) {
   let assertionPassed = `💚💚💚Assertion Passed: ${actual} === ${expected}`;
   let assertionFailed = `🅾️🅾️🅾️Assertion Failed: ${actual} !== ${expected}`;
