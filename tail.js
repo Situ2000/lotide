47eaa2efbd179c70ea76eda73f4700e6b60bd3ee
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 10:11:09 +0000

    Add comments for tail

--- tail.js
+++ tail.js
@@ -1,5 +1,6 @@
 const tail = function(arr) {
   let result = [];
+  // Return all the items in the array except for the first one
   for (let i = 1; i < arr.length; i++) {
     result.push(arr[i]);
   }
