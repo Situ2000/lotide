dfafb1d9b1e97dba2a434c6be7a14ea3cf7ae879
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 10:09:23 +0000

    Add comments for middle

--- middle.js
+++ middle.js
@@ -2,6 +2,9 @@ const middle = function(arr) {
   if (arr.length < 3) {
     return [];
   } else {
+    // Return the middle items of the array
+    // If the length of the array is odd, then return only one item
+    // If the length of the array is even, then return two items
     const evenMedium1 = arr[arr.length / 2 - 1]
     const evenMedium2 = arr[arr.length / 2]
     const oddMedium = arr[Math.floor(arr.length / 2)]
