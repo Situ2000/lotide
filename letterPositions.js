5511fd415f7d05eae51590ae4d0dc99731f36035
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 10:04:02 +0000

    Add comments for letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -4,6 +4,7 @@ const letterPositions = function(sentence) {
   for (let i = 0; i < sentence.length; i++) {
     const letter = sentence[i];
     
+    // For each letter in the sentence, add its index to the result
     if (letter !== " ") {
       if (results[letter]) {
         results[letter].push(i);
