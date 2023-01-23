82a8934bbb7656ae97ec95ff17a4bada4a055701
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 09:22:08 +0000

    Add comments for countOnly

--- countOnly.js
+++ countOnly.js
@@ -2,6 +2,7 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
+    // If the value of the key in itemsToCount is true, count the number of the key in allItems
     if (itemsToCount[item]) { 
       if (results[item]) {
         results[item] += 1;
