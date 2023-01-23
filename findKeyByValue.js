9dd180fce5081380cf7250f13f5609d80bb5bf85
Situ2000 <yuanyuansitu@gmail.com>
2023-01-23 09:53:38 +0000

    Add comments for findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,5 @@
 const findKeyByValue = function(sampleObject, value) {
+  // Compare each value in sampleObject to the given value, return the key when they are equal
   for (const key in sampleObject) {
     if (sampleObject[key] === value) {
       return key;
